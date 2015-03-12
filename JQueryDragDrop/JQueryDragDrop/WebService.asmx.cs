51224490c21c454063f9767470468a32c30a525a
smaslam <shmoaslam@gmail.com>
2015-03-12 14:54:17 +0530

    uploading

---
+++ JQueryDragDrop/JQueryDragDrop/WebService.asmx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+
+namespace JQueryDragDrop
+{
+    /// <summary>
+    /// Summary description for WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public void PrintData(Data[] d)
+        {
+            //return Data;
+        }
+        [Serializable]
+        public class Data 
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+    }
+}
